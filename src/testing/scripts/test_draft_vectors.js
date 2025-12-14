f3b73e0d2bac784824c6d66bc1a36d31276119c4
Abel <abel@minddojo.org>
2025-12-13 19:43:55 -0800

    fixing lint error

--- src/testing/scripts/test_draft_vectors.js
+++ src/testing/scripts/test_draft_vectors.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 const { Pinecone } = require('@pinecone-database/pinecone');
 
 // Initialize Pinecone client
--- src/testing/scripts/test_pipeline_database.js
+++ src/testing/scripts/test_pipeline_database.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+/* eslint-disable @typescript-eslint/no-require-imports */
 
 /**
  * Isolated Test Script for Pipeline Database Saving
